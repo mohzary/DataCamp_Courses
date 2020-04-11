9a0edbb84b4734d1f82b2e309da69889bab83b91
MUHAMMAD MOHZARY <mmohzary@kent.edu>
2020-04-11 18:32:03 -0500

    Create visualization.py

---
+++ Big_Data_PySpark/PySpark_SQL_DataFrames/visualization.py
@@ -0,0 +1,45 @@
+# Check the column names of names_df
+print("The column names of names_df are", names_df.columns)
+
+# Convert to Pandas DataFrame  
+df_pandas = names_df.toPandas()
+
+# Create a horizontal bar plot
+df_pandas.plot(kind='barh', x='Name', y='Age', colormap='winter_r')
+plt.show()
+
+#========================================================================
+
+# Load the Dataframe
+fifa_df = spark.read.csv(file_path, header=True, inferSchema=True)
+
+# Check the schema of columns
+fifa_df.printSchema()
+
+# Show the first 10 observations
+fifa_df.show(10)
+
+# Print the total number of rows
+print("There are {} rows in the fifa_df DataFrame".format(fifa_df.count()))
+
+#============================================================================
+
+# Create a temporary view of fifa_df
+fifa_df.createOrReplaceTempView('fifa_df_table')
+
+# Construct the "query"
+query = '''SELECT Age FROM fifa_df_table WHERE Nationality == "Germany"'''
+
+# Apply the SQL "query"
+fifa_df_germany_age = spark.sql(query)
+
+# Generate basic statistics
+fifa_df_germany_age.describe().show()
+
+#================================================================================
+# Convert fifa_df to fifa_df_germany_age_pandas DataFrame
+fifa_df_germany_age_pandas = fifa_df_germany_age.toPandas()
+
+# Plot the 'Age' density of Germany Players
+fifa_df_germany_age_pandas.plot(kind='density')
+plt.show()
