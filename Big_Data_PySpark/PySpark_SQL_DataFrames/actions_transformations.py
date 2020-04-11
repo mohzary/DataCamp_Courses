87dfb11a39496b9e77d6a425a56991b39f8161a9
MUHAMMAD MOHZARY <mmohzary@kent.edu>
2020-04-11 16:34:39 -0500

    Create actions_transformations.py

---
+++ Big_Data_PySpark/PySpark_SQL_DataFrames/actions_transformations.py
@@ -0,0 +1,33 @@
+# Print the first 10 observations 
+people_df.show(10)
+
+# Count the number of rows 
+print("There are {} rows in the people_df DataFrame.".format(people_df.count()))
+
+# Count the number of columns and their names
+print("There are {} columns in the people_df DataFrame and their names are {}".format(len(people_df.columns), people_df.columns))
+
+#================================================================
+# Select name, sex and date of birth columns
+people_df_sub = people_df.select('name', 'sex', 'date of birth')
+
+# Print the first 10 observations from people_df_sub
+people_df_sub.show(10)
+
+# Remove duplicate entries from people_df_sub
+people_df_sub_nodup = people_df_sub.dropDuplicates()
+
+# Count the number of rows
+print("There were {} rows before removing duplicates, and {} rows after removing duplicates".format(people_df_sub.count(), people_df_sub_nodup.count()))
+
+
+#======================================================================
+
+# Filter people_df to select females 
+people_df_female = people_df.filter(people_df.sex == "female")
+
+# Filter people_df to select males
+people_df_male = people_df.filter(people_df.sex == "male")
+
+# Count the number of rows 
+print("There are {} rows in the people_df_female DataFrame and {} rows in the people_df_male DataFrame".format(people_df_female.count(), people_df_male.count()))
